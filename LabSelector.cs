a5fcce3872b1deb2eeff7ed595d7f335a882db06
Armando <armanela@bk.ru>
2025-03-31 04:04:32 +0500

    Lab5

--- LabSelector.cs
+++ LabSelector.cs
@@ -11,6 +11,7 @@ namespace CourseProject
                 2 => new Lab2.Lab2Window(),
                 3 => new Lab3.Lab3Window(),
                 4 => new Lab4.Lab4Window(),
+                5 => new Lab5.Lab5Window(),
                 _ => null
             };
 
